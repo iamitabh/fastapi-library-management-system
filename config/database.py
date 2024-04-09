b7ebc7a608c663c02abab824fa5c427fa9fecf42
Amitabh Mishra <115503862+iamitabh@users.noreply.github.com>
2024-04-09 09:45:25 +0530

    Update database.py

--- config/database.py
+++ config/database.py
@@ -1,6 +1,6 @@
 from pymongo import MongoClient
 
-client=MongoClient('mongodb+srv://amitabhmishra2002:HdrvshmXn7ZzLfmE@cluster0.7mahv0o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+client=MongoClient('mongodb+srv://<UserName>:<Password>@cluster0.7mahv0o.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 
 db=client.library_db
 
